033867509c6794860a969850aae9b07d21d38c3a
br14nn <vituallabrian727@gmail.com>
2026-04-18 03:24:45 +0800

    chore: setup shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="h-svh min-h-fit w-full bg-neut-50"></div>;
+  return (
+    <div className="h-svh min-h-fit w-full bg-neut-50">
+      <h1 className="text-black">Test your components below</h1>
+    </div>
+  );
 }
 
 export default App;
